f43253cbeff9e059f0ec30dad8a93802932e7357
Luiz Augusto Scarsi <luizaugustoscarsi@gmail.com>
2025-12-14 14:38:43 -0300

    feat: add admin account creation

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,74 +1,16 @@
-// import { useState } from "react";
-// import { api } from "../services/api";
-
-// export default function Login({ onLogin }: { onLogin: () => void }) {
-//   const [email, setEmail] = useState("");
-//   const [password, setPassword] = useState("");
-//   const [error, setError] = useState("");
-
-//   const handleLogin = async (e: React.FormEvent) => {
-//     e.preventDefault();
-//     setError("");
-
-//     try {
-//       const res = await api.post("/auth/login", { email, password });
-
-//       // Exemplo: guarde o token
-//       localStorage.setItem("token", res.data.token);
-
-//       onLogin(); // <-- chama o App para liberar acesso
-//     } catch (err) {
-//       setError("Credenciais inválidas.");
-//     }
-//   };
-
-//   return (
-//     <div className="d-flex justify-content-center align-items-center w-100 min-vh-100">
-//       <div className="card p-4 shadow" style={{ maxWidth: 350, width: "100%" }}>
-//         <h2 className="mb-3">Login</h2>
-
-//         {error && <p className="text-danger">{error}</p>}
-
-//         <form onSubmit={handleLogin}>
-//           <div className="mb-3">
-//             <input
-//               type="email"
-//               className="form-control"
-//               placeholder="E-mail"
-//               value={email}
-//               onChange={(e) => setEmail(e.target.value)}
-//               required
-//             />
-//           </div>
-
-//           <div className="mb-3">
-//             <input
-//               type="password"
-//               className="form-control"
-//               placeholder="Senha"
-//               value={password}
-//               onChange={(e) => setPassword(e.target.value)}
-//               required
-//             />
-//           </div>
-
-//           <button className="btn btn-primary w-100" type="submit">
-//             Entrar
-//           </button>
-//         </form>
-//       </div>
-//     </div>
-//   );
-// }
-import { useState } from "react";
+import { use, useState } from "react";
 import { api } from "../services/api";
+import { useAuth } from "../contexts/AuthContext.tsx";
 
 export default function Login({ onLogin }: { onLogin: () => void }) {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [name, setName] = useState("");
+  const [role, setRole] = useState("");
   const [error, setError] = useState("");
   const [mode, setMode] = useState<"login" | "register">("login");
+
+  const {login} = useAuth();
 
   const generateId = () => Date.now();
 
@@ -79,7 +21,7 @@ export default function Login({ onLogin }: { onLogin: () => void }) {
     try {
       if (mode === "login") {
         const res = await api.post("/auth/login", { email, password });
-        localStorage.setItem("token", res.data.token);
+        login(res.data.token, res.data.name, res.data.role);
         onLogin();
       } else {
         await api.post("/user", {
@@ -87,7 +29,7 @@ export default function Login({ onLogin }: { onLogin: () => void }) {
           name,
           email,
           password,
-          role: "user",
+          role,
         });
 
         setMode("login");
@@ -142,6 +84,19 @@ export default function Login({ onLogin }: { onLogin: () => void }) {
               required
             />
           </div>
+
+          {mode === "register" && (
+            <div className="mb-3">
+              <input
+                type="role"
+                className="form-control"
+                placeholder="Função"
+                value={role}
+                onChange={(e) => setRole(e.target.value)}
+                required
+              />
+            </div>
+          )}
 
           <button className="btn btn-primary w-100" type="submit">
             {mode === "login" ? "Entrar" : "Cadastrar"}
